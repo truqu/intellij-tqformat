08a3baacc8f562bf71b8eafaf565039e9809b409
ilias <mail@ilias.xyz>
2020-08-11 11:44:41 +0200

    Show tqformat version in config panel

--- src/main/kotlin/com/truqu/intellijtqformat/TqFormatConfiguration.kt
+++ src/main/kotlin/com/truqu/intellijtqformat/TqFormatConfiguration.kt
@@ -1,15 +1,28 @@
 package com.truqu.intellijtqformat
 
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.CapturingProcessHandler
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.layout.panel
+import com.intellij.util.Alarm
+import com.intellij.util.ui.UIUtil
 import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 import kotlin.reflect.KProperty
+import org.jetbrains.annotations.Nullable
 
-class TqFormatConfiguration(private val project: Project) : Configurable {
+private const val DELAY_MILLIS = 200
+
+class TqFormatConfiguration(private val project: Project) : Configurable, @Nullable Disposable {
     private val settings = TqFormatSettings(project)
 
     private val tqFormatPathField = TextFieldWithBrowseButton().apply {
@@ -19,8 +32,24 @@ class TqFormatConfiguration(private val project: Project) : Configurable {
             project,
             FileChooserDescriptorFactory.createSingleFileNoJarsDescriptor()
         )
+        text = settings.path
+        childComponent.document.addDocumentListener(object : DocumentListener {
+            override fun changedUpdate(e: DocumentEvent?) = changed()
+            override fun insertUpdate(e: DocumentEvent?) = changed()
+            override fun removeUpdate(e: DocumentEvent?) = changed()
+            private fun changed() {
+                tqformatPathInput = childComponent.text
+            }
+        })
     }
-    private var tqformatPathInput: String by PathSelectorDelegate(tqFormatPathField)
+    private var tqformatPathInput: String = settings.path
+        set(value) {
+            field = value
+            updateVersion()
+        }
+    private val tqformatVersionField = JLabel().apply {
+        foreground = UIUtil.getContextHelpForeground()
+    }
 
     private val onSaveCheckbox = JBCheckBox("Format on save")
     private var onSaveChecked: Boolean by CheckboxDelegate(onSaveCheckbox)
@@ -51,19 +80,36 @@ class TqFormatConfiguration(private val project: Project) : Configurable {
                     """.trimIndent()
                 )
             }
-            row("Path to tqformat executable") { tqFormatPathField() }
+            row("Path to tqformat executable") { tqFormatPathField(); tqformatVersionField() }
             row {
                 onSaveCheckbox().comment("Format all Erlang files when persisting to disk")
             }
         }
+
+    private fun updateVersion() {
+        val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, this)
+        alarm.cancelAllRequests()
+        alarm.addRequest(
+            {
+                val r = findVersion(getEScriptPath(project), tqformatPathInput)
+                invokeLater(ModalityState.any()) {
+                    tqformatVersionField.text = r
+                }
+            },
+            DELAY_MILLIS
+        )
+    }
+
+    override fun dispose() = Unit
 }
 
-private class PathSelectorDelegate(private val pathSelector: TextFieldWithBrowseButton) {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): String = pathSelector.text
-
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        pathSelector.text = value
-    }
+private fun findVersion(escriptPath: String?, tqformatPath: String): String {
+    if (escriptPath == null) return "No SDK"
+    val cli = GeneralCommandLine(escriptPath)
+        .withParameters(tqformatPath, "--version")
+    val handler = CapturingProcessHandler(cli)
+    val result = handler.runProcess()
+    return if (result.exitCode == 0) result.stdout else "Invalid"
 }
 
 private class CheckboxDelegate(private val checkbox: JBCheckBox) {
